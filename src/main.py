2bfbbd04bc7fc895a1c25e6adc8d5a2d7c7df8cd
evelyn <violetmoonlightmj@gmail.com>
2023-03-13 15:42:42 +0330

    init

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-
-
+import random
 import discord
 from discord.ext.commands import Bot, check, Context
 
-TOKEN = open("../TOKEN", "r")
+TOKEN = open("TOKEN", "r")
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = Bot(intents=intents, command_prefix="!")
+client = Bot(intents=intents, command_prefix="*")
 
 
 @client.event
@@ -17,8 +16,8 @@ async def on_ready():
     print(f'Logged on as {client.user}!')
 
 @client.command(pass_context=True, invoke_without_command=True)
-async def ping(context: Context):
-    await context.reply("hello im not a bad person and i dont swear")
+async def doxx(context: Context, arg):
+    await context.reply(f"{arg}'s ip is {random.randint(10,255)}.{random.randint(10,255)}.{random.randint(10,255)}.{random.randint(10,255)}")
 
 
 
